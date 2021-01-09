e11f8a41add605e39a32f3f0a203aa544a1c2607
animeshdas2000 <animeshxdas@gmail.com>
2021-01-09 21:26:04 +0530

    signup

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const index_controller = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ARG' });
+  
 });
 
 router.get ("/register", index_controller.register);

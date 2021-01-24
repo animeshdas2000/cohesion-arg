5b7c3c9c9a80025d0b473b3fc580c58d499eed4e
Akshay Makkar <akshaym1103@gmail.com>
2021-01-24 18:57:17 +0530

    Production firebaseconfig

--- config.js
+++ config.js
@@ -20,14 +20,28 @@
 //     }
 // }
 
+// DEVELOPMENT - cohesion2021@gmail.com
+// module.exports = {
+//   firebaseConfig: {
+//     apiKey: 'AIzaSyBkJBgfFK9_YkwilwdKnhiqY7RFh8AvtEM',
+//     authDomain: 'cohesion-c4eed.firebaseapp.com',
+//     projectId: 'cohesion-c4eed',
+//     storageBucket: 'cohesion-c4eed.appspot.com',
+//     messagingSenderId: '626347142382',
+//     appId: '1:626347142382:web:6cae6a68d37c7f5e1997ff',
+//     measurementId: 'G-N3PYDNB14V',
+//   },
+// };
+
+//PRODUCTION - cohesion2021@gmail.com
 module.exports = {
   firebaseConfig: {
-    apiKey: 'AIzaSyBkJBgfFK9_YkwilwdKnhiqY7RFh8AvtEM',
-    authDomain: 'cohesion-c4eed.firebaseapp.com',
-    projectId: 'cohesion-c4eed',
-    storageBucket: 'cohesion-c4eed.appspot.com',
-    messagingSenderId: '626347142382',
-    appId: '1:626347142382:web:6cae6a68d37c7f5e1997ff',
-    measurementId: 'G-N3PYDNB14V',
-  },
-};
+    apiKey: "AIzaSyAp3knh5rFd_B0m240P59laXXfNYxET7no",
+    authDomain: "cohesion-production.firebaseapp.com",
+    projectId: "cohesion-production",
+    storageBucket: "cohesion-production.appspot.com",
+    messagingSenderId: "881897441209",
+    appId: "1:881897441209:web:fa5d450e104fbdb51ed868",
+    measurementId: "G-WW9E9FNYPZ"
+  }
+}
